b3ed00f6833e49e84567502f29a918af6ef6a28e
g7crservice-yalla-prasad <yalla.prasad@g7cr.com>
2025-10-27 14:43:35 +0530

    Create environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+// export const environment = {
+// 	APP_UI_URL: 'http://localhost:3000/',
+//     APP_NAME: 'MY-REACT-APP'
+// };
+
+export const environment:any = {
+	APP_UI_URL: `{{APP_UI_URL}}`,
+    APP_NAME: `{APP_NAME}`
+};
